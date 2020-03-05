8aa2de104d80ffc836880d0b4f527b6a75710883
Samuel <samuelvvelzen@gmail.com>
2020-03-05 08:50:23 +0100

    playing with requests and beautifulsoup

--- scrapping.py
+++ scrapping.py
@@ -3,7 +3,17 @@ import urllib.request
 import time
 from bs4 import BeautifulSoup
 
-URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Australia'
-page = requests.get(URL)
+import config
+import re
 
-soup = BeautifulSoup(page.content, 'html.parser')
+url = config.target["mainUrl"] + config.target["listUrl"]
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+# print(soup.findAll(['div.subtopics', 'ul', 'li', 'a']))
+
+# parentDiv = soup.findAll("div", class_="subtopics")
+# print(parentDiv)
+
+print(soup.findAll(re.compile("div")))
+# print(soup.findAll(href="/topics/embassies-consulates-and-other-representations/overview-countries-and-regions/liberia"))
